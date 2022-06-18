6fd72b2ad673159bf65b2feedb3ac1a6a5153115
polashrahim <polashrahim25@gmail.com>
2022-06-18 11:18:04 +0600

    initial master

---
+++ main.js
@@ -0,0 +1,46 @@
+const profile =['saim','Hasan', 25, 'Web developer', true]
+profile[3]
+'Web developer'
+profile[1] + profile[2]
+'Hasan25'
+profile[3] + ' ' + profile[2]
+'Web developer 25'
+profile[6]='Bangladesh'
+'Bangladesh'
+
+
+const demoprofile={
+    firstName:'samim',
+    age:25,
+    profession:'Webdeveloper',
+
+};
+
+const profile={}
+profile.name="Travis";
+profile.age=21;
+profile.city="LA";
+
+const person={
+    name:'Rahim',
+    age:30,
+    city:'Dhaka'
+};
+
+const profile=new Object();
+profile.name='Rahim';
+profile.age=30;
+profile.city='BD';
+
+//Object Excercise
+const exercise={
+    name: 'samim',
+    age: 25,
+    ocpation:'developer',
+    choice:'javascript'
+
+
+};
+
+
+
